3f2d6b3d914b2e513745e2fcefab2e6f6cab8809
nbaugh1 <nbaugh1@gmail.com>
2020-03-23 17:59:25 -0400

    refactor using Layout and Header components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { useSiteMetadata } from '../hooks/useSiteMetadata';
-import styled from 'styled-components';
+import { Layout } from '../components/Layout'
 
 export default () => {
-  const StyledH1 = styled.h1 `
-    color: #34403A
-  `
-  const { title, description } = useSiteMetadata();
+
   return (
     <>
-      <StyledH1>{title}</StyledH1>
-      <p>{description}</p>
+      <Layout />
     </>
   );
 };
