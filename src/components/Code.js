321add3900088ea9743c14b2a78ec0aa34c522c4
nbaugh1 <nbaugh1@gmail.com>
2020-03-24 19:14:41 -0400

    add styling to code blocks

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,7 +1,30 @@
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/nightOwl';
 import React from 'react';
-const Code = ({ codeString, language }) => {
+import styled from 'styled-components';
+
+export const Pre = styled.pre`
+  text-align: left;
+  margin: 1em 0;
+  padding: 0.5em;
+  overflow-x: auto;
+  border-radius: 3px;
+
+  & .token-line {
+    line-height: 1.3em;
+    height: 1.3em;
+  }
+  font-family: 'Courier New', Courier, monospace;
+`;
+
+export const LineNo = styled.span`
+  display: inline-block;
+  width: 2em;
+  user-select: none;
+  opacity: 0.3;
+`;
+
+const Code = ({ codeString, language, ...props }) => {
   return (
     <Highlight
       {...defaultProps}
@@ -16,17 +39,19 @@ const Code = ({ codeString, language }) => {
         getLineProps,
         getTokenProps,
       }) => (
-        <pre className={className} style={style}>
+        <Pre className={className} style={style}>
           {tokens.map((line, i) => (
             <div {...getLineProps({ line, key: i })}>
+              <LineNo>{i + 1}</LineNo>
               {line.map((token, key) => (
                 <span {...getTokenProps({ token, key })} />
               ))}
             </div>
           ))}
-        </pre>
+        </Pre>
       )}
     </Highlight>
   );
 };
+
 export default Code;
